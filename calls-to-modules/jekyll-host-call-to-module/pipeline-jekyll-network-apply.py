0b3c966df668d92a82edf5b21b5d56534a40e475
JimGreenRiverIT <51681501+JimGreenRiverIT@users.noreply.github.com>
2020-04-14 14:11:24 -0700

    Update pipeline-jekyll-network-apply.py

--- calls-to-modules/jekyll-host-call-to-module/pipeline-jekyll-network-apply.py
+++ calls-to-modules/jekyll-host-call-to-module/pipeline-jekyll-network-apply.py
@@ -1,6 +1,16 @@
 import subprocess
+import os 
 
 path_to_jekyll_iam_keys="C:\\projects\\Jekyll\\vars\\VarsForTerraform\\"
+
+print("..inside pipeline jekyll network apply")
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+cwd = os.getcwd()
+print("dir_path is: ")
+print(dir_path)
+print("cwd is: ")
+print(cwd)
 
 subprocess.run("terraform init", shell=True, check=True)
 
