dae6d3b99cfb931cc782fef64aed7dd3fe543792
JimGreenRiverIT <51681501+JimGreenRiverIT@users.noreply.github.com>
2020-04-17 13:00:23 -0700

    Update pipeline-jekyll-network-apply.py

--- calls-to-modules/jekyll-host-call-to-module/pipeline-jekyll-network-apply.py
+++ calls-to-modules/jekyll-host-call-to-module/pipeline-jekyll-network-apply.py
@@ -13,11 +13,6 @@ print(dir_path)
 print("cwd is: ")
 print(cwd)
 
-#print("The imported variables resolve to: ")
-#print(sys.argv[0])
-#print(sys.argv[1])
-#print(sys.argv[2])
-
 print("The new environment variables read into python pipeline-jekyll-network-apply are: ")
 print(os.environ['P_K_A'])
 print(os.environ['S_K_A'])
@@ -35,15 +30,12 @@ print("About to interpolate skvar: ")
 print(f'-var=\"_secret_access_key={skvar}\"')
 print("About to print skstr: ")
 print(skstr)
- 
-
-#-var="name_of_ssh_key=ansible-server" 
-#-var="_public_access_key=$()" 
-#-var="_secret_access_key=$(-secret-access-key)" 
-#-var="aws_region=us-west-2"
+argsstr = f'-var=\"name_of_ssh_key=ansible-server\" {pkstr} {skstr} -var=\"aws_region=us-west-2\"'
+print("About to print argsstr: ")
+print(argsstr)
 
 subprocess.run("terraform init", shell=True, check=True)
 
-subprocess.run('terraform apply -auto-approve -var="name_of_ssh_key=ansible-server" ' + pkstr + ' ' + skstr + ' -var="aws_region=us-west-2"', shell=True, check=True)
+#PUT THIS BACK IN: subprocess.run('terraform apply -auto-approve -var="name_of_ssh_key=ansible-server" ' + pkstr + ' ' + skstr + ' -var="aws_region=us-west-2"', shell=True, check=True)
 
 #subprocess.run("terraform apply -auto-approve -var-file="+path_to_jekyll_iam_keys+"awspublickey.tfvars -var-file="+path_to_jekyll_iam_keys+"awsvpcmeta.tfvars -var-file="+path_to_jekyll_iam_keys+"awskeymeta.tfvars", shell=True, check=True)
