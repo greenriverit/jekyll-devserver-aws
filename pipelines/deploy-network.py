a61c368f50f1ec5ef3da2cb8b319d5d2a62f5c4a
JimGreenRiverIT <51681501+JimGreenRiverIT@users.noreply.github.com>
2020-04-17 11:51:02 -0700

    Update deploy-network.py

--- pipelines/deploy-network.py
+++ pipelines/deploy-network.py
@@ -19,20 +19,14 @@ print(os.environ['S_K_A'])
 #path_to_jekyll_module='C:\\projects\\terraform\\portable-kubernetes-example\\modules\\ansible-and-aws-hostnetwork-module\\'
 #path_to_call_to_jekyll_module = "C:\\projects\\terraform\\jekyll-devbox-aws\\calls-to-modules\\jekyll-host-call-to-module"
 path_to_call_to_jekyll_module = "../calls-to-modules/jekyll-host-call-to-module"
-command_to_call_jekyll_module = 'python3 pipeline-jekyll-network-apply.py '
-#+ sys.argv[1] + ' ' + sys.argv[2]
-
-#print("The imported variables resolve to: ")
-#print(sys.argv[0])
-#print(sys.argv[1])
-#print(sys.argv[2])
+command_to_call_jekyll_module = 'python3 pipeline-jekyll-network-apply.py'
 
 myRegion="us-west-2"
 
 ################################################################################  
 ### 1. NOW DEPLOY THE JEKYLL HOST NETWORK
 ################################################################################  
-#PUT THIS BACK IN FOR REAL: ndep.deployHostNetwork( command_to_call_jekyll_module, path_to_call_to_jekyll_module)
+ndep.deployHostNetwork( command_to_call_jekyll_module, path_to_call_to_jekyll_module)
 #PUT THIS BACK IN FOR REAL: nval.validateHostNetwork(ndep.cidr_subnet, ndep.cidr_subnet_list, ndep.security_group_id, ndep.vpc_id, ndep.route_table_id)
 print("                                  ")
 print("  **** Finished Deploying Jekyll Host Network. ****")
