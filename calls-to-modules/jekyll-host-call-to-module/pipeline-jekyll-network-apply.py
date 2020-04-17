e82389bf80b6040d00855821c7b593f0b3307799
JimGreenRiverIT <51681501+JimGreenRiverIT@users.noreply.github.com>
2020-04-17 13:41:08 -0700

    Update pipeline-jekyll-network-apply.py

--- calls-to-modules/jekyll-host-call-to-module/pipeline-jekyll-network-apply.py
+++ calls-to-modules/jekyll-host-call-to-module/pipeline-jekyll-network-apply.py
@@ -33,14 +33,14 @@ print(skstr)
 argsstr = f'terraform apply -auto-approve -var=\"name_of_ssh_key=ansible-server\" {pkstr} {skstr} -var=\"aws_region=us-west-2\"'
 print("About to print argsstr: ")
 print(argsstr)
-skstr = f'-var=\"_secret_access_key={skvar}\"'
-cmdstr = f'__terraform_apply -auto-approve {argstr}'
-print("About to print cmdstr: ")
-print(cmdstr) 
+#skstr = f'-var=\"_secret_access_key={skvar}\"'
+#cmdstr = f'__terraform_apply -auto-approve {argstr}'
+#print("About to print cmdstr: ")
+#print(cmdstr) 
 
 subprocess.run("terraform init", shell=True, check=True)
 
-#subprocess.run(cmdstr, shell=True, check=True)
+subprocess.run(argsstr, shell=True, check=True)
 
 
 #subprocess.run('terraform apply -auto-approve -var="name_of_ssh_key=ansible-server" ' + pkstr + ' ' + skstr + ' -var="aws_region=us-west-2"', shell=True, check=True)
