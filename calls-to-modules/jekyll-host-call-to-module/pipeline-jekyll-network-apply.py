823f6a108ed04e608f31d6cbbc5b53c6dc8a3fd6
JimGreenRiverIT <51681501+JimGreenRiverIT@users.noreply.github.com>
2020-04-17 12:13:30 -0700

    Update pipeline-jekyll-network-apply.py

--- calls-to-modules/jekyll-host-call-to-module/pipeline-jekyll-network-apply.py
+++ calls-to-modules/jekyll-host-call-to-module/pipeline-jekyll-network-apply.py
@@ -13,14 +13,36 @@ print(dir_path)
 print("cwd is: ")
 print(cwd)
 
-print("The imported variables resolve to: ")
-print(sys.argv[0])
-print(sys.argv[1])
-print(sys.argv[2])
+#print("The imported variables resolve to: ")
+#print(sys.argv[0])
+#print(sys.argv[1])
+#print(sys.argv[2])
 
+print("The new environment variables read into python pipeline-jekyll-network-apply are: ")
+print(os.environ['P_K_A'])
+print(os.environ['S_K_A'])
+
+pkvar = os.environ['P_K_A']
+pkstr = f'_public_access_key={pkvar}'
+print("About to interpolate pkvar: ")
+print(f'_public_access_key={pkvar}')
+print("About to print pkstr: ")
+print(pkstr)
+
+skvar = os.environ['S_K_A']
+skstr = f'_secret_access_key={skvar}'
+print("About to interpolate skvar: ")
+print(f'_secret_access_key={skvar}')
+print("About to print skstr: ")
+print(skstr)
+
+#-var="name_of_ssh_key=ansible-server" 
+#-var="_public_access_key=$()" 
+#-var="_secret_access_key=$(-secret-access-key)" 
+#-var="aws_region=us-west-2"
 
 subprocess.run("terraform init", shell=True, check=True)
 
-subprocess.run('terraform apply -auto-approve -var="name_of_ssh_key=ansible-server" -var="_public_access_key=$(-public-access-key)" -var="_secret_access_key=$(-secret-access-key)" -var="aws_region=us-west-2"', shell=True, check=True)
+#MODIFY THIS LINE AFTER VALIDATING STRINGS FIRST:  subprocess.run('terraform apply -auto-approve -var="name_of_ssh_key=ansible-server" -var="_public_access_key=$()" -var="_secret_access_key=$(-secret-access-key)" -var="aws_region=us-west-2"', shell=True, check=True)
 
 #subprocess.run("terraform apply -auto-approve -var-file="+path_to_jekyll_iam_keys+"awspublickey.tfvars -var-file="+path_to_jekyll_iam_keys+"awsvpcmeta.tfvars -var-file="+path_to_jekyll_iam_keys+"awskeymeta.tfvars", shell=True, check=True)
