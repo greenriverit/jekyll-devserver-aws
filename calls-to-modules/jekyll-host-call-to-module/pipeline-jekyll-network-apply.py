ac11a98978fc5259685c4fca0dbf368f95785e3e
JimGreenRiverIT <51681501+JimGreenRiverIT@users.noreply.github.com>
2020-04-17 14:21:38 -0700

    Update pipeline-jekyll-network-apply.py

--- calls-to-modules/jekyll-host-call-to-module/pipeline-jekyll-network-apply.py
+++ calls-to-modules/jekyll-host-call-to-module/pipeline-jekyll-network-apply.py
@@ -38,8 +38,8 @@ subprocess.run("terraform init", shell=True, check=True)
 
 try:
     print("Inside __try__ block in __pipeline-jekyll-network-apply__.  About to run __terraform__apply command. ")
-    #subprocess.run(argsstr, shell=True, check=True)
-    throwanerror! 
+    subprocess.run(argsstr, shell=True, check=True)
+    #throwanerror! 
 except Exception: 
     print("FAILED TO RUN THE argsstr COMMAND. ")
     pass
