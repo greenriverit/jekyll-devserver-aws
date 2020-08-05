30437864b577a93703621279573ca573113dc62c
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-05 14:20:35 -0700

    Update remove-network.py

--- pipelines/remove-network.py
+++ pipelines/remove-network.py
@@ -1,3 +1,7 @@
+## Copyright 2020 Green River IT (GreenRiverIT.com) as described in LICENSE.txt distributed with this project on GitHub.  
+## Start at https://github.com/GreenRiverIT    
+
+
 import subprocess
 import re
 import networkdeploymentfunctions as ndf
