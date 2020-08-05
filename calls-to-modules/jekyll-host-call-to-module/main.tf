b87898b6bdffeff0182a666489135c2e79cb9438
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-05 14:10:15 -0700

    Update main.tf

--- calls-to-modules/jekyll-host-call-to-module/main.tf
+++ calls-to-modules/jekyll-host-call-to-module/main.tf
@@ -4,16 +4,16 @@ module "jekyll-devbox" {
   _region = "us-west-2"
   access_key = "${var._public_access_key}"
   secret_access_key = "${var._secret_access_key}"
-  path_to_ssh_keys = "${var.path_to_ssh_keys}"
-  name_of_ssh_key = "${var.name_of_ssh_key}"
+  # path_to_ssh_keys = "${var.path_to_ssh_keys}"
+  # name_of_ssh_key = "${var.name_of_ssh_key}"
 
 }
 
 variable "aws_region" { }
 variable "_public_access_key" { }
 variable "_secret_access_key" { }
-variable "path_to_ssh_keys" { }
-variable "name_of_ssh_key" { }
+# variable "path_to_ssh_keys" { }
+# variable "name_of_ssh_key" { }
 
 ##Output variables
 output "security_group_id_jekyll_hosts" { value = "${module.jekyll-devbox.security_group_id_jekyll_hosts}" }
