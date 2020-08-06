37f9b886762689f1414b99e1aa97366f22a5adcf
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-06 13:17:53 -0700

    Delete pipeline-jekyll-network-destroy.py

--- calls-to-modules/jekyll-host-call-to-module/pipeline-jekyll-network-destroy.py
+++
@@ -1,8 +0,0 @@
-## Copyright 2020 Green River IT (GreenRiverIT.com) as described in LICENSE.txt distributed with this project on GitHub.  
-## Start at https://github.com/GreenRiverIT    
-
-import subprocess
-
-path_to_jekyll_iam_keys="C:\\projects\\Jekyll\\vars\\VarsForTerraform\\"
-
-subprocess.run("terraform destroy -auto-approve -var-file="+path_to_jekyll_iam_keys+"awspublickey.tfvars -var-file="+path_to_jekyll_iam_keys+"awsvpcmeta.tfvars -var-file="+path_to_jekyll_iam_keys+"awskeymeta.tfvars", shell=True, check=True)
