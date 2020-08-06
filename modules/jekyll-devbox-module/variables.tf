500d9a5f6cb3fb120d62cfda2a566572c9eb76f7
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-06 13:04:21 -0700

    Update variables.tf

--- modules/jekyll-devbox-module/variables.tf
+++ modules/jekyll-devbox-module/variables.tf
@@ -11,6 +11,10 @@ variable "_region" { }
 variable "path_to_ssh_keys" { }
 variable "name_of_ssh_key" { }
 
+#VPC to which the security group will be attached
+variable "vpc_id" {}
+data "aws_vpc" "selected" { id = var.vpc_id }
+
 # Workstation External IP. Override with variable or hardcoded value if necessary.
 data "http" "admin-external-ip" { url = "http://ipv4.icanhazip.com" }
 locals { admin-external-cidr = "${chomp(data.http.admin-external-ip.body)}/32" }
@@ -20,4 +24,3 @@ output "security_group_id_jekyll_hosts" { value = "${aws_security_group.jekyll-h
 output "vpc_id_jekyll" { value = "${aws_vpc.jekyll-host.id}" }
 output "cidr_subnet_jekyll" { value = "${aws_subnet.jekyll-host.cidr_block}" }
 output "route_table_id_jekyll_host" { value = "${aws_route_table.jekyll-host.id}" }
-
