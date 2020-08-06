4b0bf52dedfae8cacc3b4b33c200f83bfa3fe630
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-06 13:24:55 -0700

    Update and rename pipelines/deploy-network.py to pipeline-tasks/createJekyllStuff.py

---
+++ pipeline-tasks/createJekyllStuff.py
@@ -0,0 +1,13 @@
+## Copyright 2020 Green River IT (GreenRiverIT.com) as described in LICENSE.txt distributed with this project on GitHub.  
+## Start at https://github.com/GreenRiverIT    
+
+import subprocess
+
+################################################################################  
+### 1. Install the module and provision for Jekyll by making a call from a pipeline
+################################################################################ 
+
+print("                                  ")
+print("  **** Logic will be added here to import variables from a pipeline and then to instantiate a module and provision for Jekyll using those variables. ****")
+print("                                  ")
+
