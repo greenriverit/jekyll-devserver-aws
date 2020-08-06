d8f57ec0b94ba0692b54908c0f38f2373b3170cf
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-06 13:29:20 -0700

    Update and rename pipelines/remove-network.py to pipeline-tasks/destroyJekyllStuff.py

---
+++ pipeline-tasks/destroyJekyllStuff.py
@@ -0,0 +1,12 @@
+## Copyright 2020 Green River IT (GreenRiverIT.com) as described in LICENSE.txt distributed with this project on GitHub.  
+## Start at https://github.com/GreenRiverIT    
+
+import subprocess
+
+################################################################################################
+### Destroy the Jekyll module and also remove any config that was used to provision for Jekyll
+################################################################################################
+
+print("                                  ")
+print("  **** Code will be added here which will remove all the Jekyll Stuff. ****")
+print("                                  ")
