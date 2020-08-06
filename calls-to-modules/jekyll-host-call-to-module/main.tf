6c263e5b14c7415a8aabd21719b4760200aa57d6
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-06 13:16:29 -0700

    Update main.tf

--- calls-to-modules/jekyll-host-call-to-module/main.tf
+++ calls-to-modules/jekyll-host-call-to-module/main.tf
@@ -4,22 +4,14 @@
 module "jekyll-devbox" {
   source = "../../modules/jekyll-devbox-module"
 
-  _region = "us-west-2"
+  _region = "${var.aws_region}"
   access_key = "${var._public_access_key}"
   secret_access_key = "${var._secret_access_key}"
-  # path_to_ssh_keys = "${var.path_to_ssh_keys}"
-  # name_of_ssh_key = "${var.name_of_ssh_key}"
+  vpc_id = "${var.vpc_id}"  
 
 }
 
 variable "aws_region" { }
 variable "_public_access_key" { }
 variable "_secret_access_key" { }
-# variable "path_to_ssh_keys" { }
-# variable "name_of_ssh_key" { }
-
-##Output variables
-output "security_group_id_jekyll_hosts" { value = "${module.jekyll-devbox.security_group_id_jekyll_hosts}" }
-output "vpc_id_jekyll" { value = "${module.jekyll-devbox.vpc_id_jekyll}" }
-output "cidr_subnet_jekyll" { value = "${module.jekyll-devbox.cidr_subnet_jekyll}" }
-output "route_table_id_jekyll_host" { value = "${module.jekyll-devbox.route_table_id_jekyll_host}" }
+variable "vpc_id" { }
