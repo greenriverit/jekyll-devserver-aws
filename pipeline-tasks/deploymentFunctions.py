3853a834f16e5af6e1b1369fbf5b7a8065b00ad9
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-09-25 14:07:46 -0700

    Create deploymentFunctions.py

---
+++ pipeline-tasks/deploymentFunctions.py
@@ -0,0 +1,19 @@
+import subprocess
+import re  
+
+ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
+
+def runShellCommand(commandToRun):
+    print("Inside runShellCommand(..., ...) function. ")
+    print("commandToRun is: " +commandToRun)
+
+    proc = subprocess.Popen( commandToRun,cwd=None, stdout=subprocess.PIPE, shell=True)
+    while True:
+      line = proc.stdout.readline()
+      if line:
+        thetext=line.decode('utf-8').rstrip('\r|\n')
+        decodedline=ansi_escape.sub('', thetext)
+        print(decodedline)
+      else:
+        break
+  
