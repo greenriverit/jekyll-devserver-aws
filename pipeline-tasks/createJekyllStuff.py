96dd48a321afbcaa8147e2884486df2a257fbd36
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-09-29 16:41:19 -0700

    Update createJekyllStuff.py

--- pipeline-tasks/createJekyllStuff.py
+++ pipeline-tasks/createJekyllStuff.py
@@ -2,6 +2,7 @@
 ## Start at https://github.com/GreenRiverIT    
 
 import subprocess
+import sys
 import deploymentFunctions as depfunc 
 
 def createHostsFileAnsible(ansibleHostsGroupName, remoteJekyllServerIP, remoteJekyllPassWord): 
@@ -9,11 +10,20 @@ def createHostsFileAnsible(ansibleHostsGroupName, remoteJekyllServerIP, remoteJe
   hostNameLine=remoteJekyllServerIP + " ansible_ssh_pass=" + remoteJekyllPassWord + "\n"	
   hostsFileNameAndPath='/etc/ansible/hosts' 
   print("hostsFileNameAndPath is: ", hostsFileNameAndPath)	
-  print("About to write 8 lines to a file.")	
+  print("About to write 2 lines to a file.")	
   f = open(hostsFileNameAndPath, "w")	
   f.write(groupNameLine)	
-  f.write(hostsFileNameAndPath)	
+  f.write(hostNameLine)	
   f.close()	
+
+def createVarsFileAnsible(varFileName, remoteJekyllPassWord):
+  pwdLine = 'root_pass_jekyll: ' + remoteJekyllPassWord
+  print("pwdLine is: ", pwdLine)
+  print("About to write 1 line to a file.")	
+  f = open(varFileName, "w")	
+  f.write(pwdLine)
+  f.close()	
+
 
 ###################################################################################  
 ### 1. Import the variables.  These will come from key vault and pipeline but for 
@@ -21,8 +31,8 @@ def createHostsFileAnsible(ansibleHostsGroupName, remoteJekyllServerIP, remoteJe
 ################################################################################### 
 DefaultWorkingDirectory=sys.argv[1] 
 remoteJekyllUserName='agile-cloud'
-remoteJekyllServerIP="12.345.6.78"
-remoteJekyllPassWord='z.z.z.z.c.v.3'
+remoteJekyllServerIP=""
+remoteJekyllPassWord=''
 ansibleHostsGroupName='demoservers'
 
 print("DefaultWorkingDirectory is:", DefaultWorkingDirectory)
@@ -45,13 +55,30 @@ depfunc.runShellCommand(knownHostsCommand)
 print("About to create the Ansible Hosts file that Ansible will use to determine which IPs to associate with each group name.  ")
 createHostsFileAnsible(ansibleHostsGroupName, remoteJekyllServerIP, remoteJekyllPassWord)
 
+hostsFileNameAndPath='/etc/ansible/hosts' 
+print("About to read the /etc/ansible/hosts file we just wrote.")	
+#open and read the file after the appending:	
+f = open(hostsFileNameAndPath, "r")	
+print(f.read()) 	
+
 ###################################################################################
 ### 4. Run the playbook
 ###################################################################################
 print("About to run the Ansible Playbook.  ")
 #First get the directory of the playbook
 playBooksDir=DefaultWorkingDirectory+"/_jekyll-devserver-aws/drop/ansible-playbooks/"
-provisionJekyllDevPlayBookNameAndPath=playBooksDir+"provisionJekyllDevServer.yaml"
-playBookCommandSlice = "ansible-playbook -v "
-provisionJekyllDevCommand = playBookCommandSlice + provisionJekyllDevPlayBookNameAndPath
+#Then create the var file which will hold the sudo password and maybe other things
+varFileName=playBooksDir+'myVars.yaml'
+print("varFileName is: ", varFileName)
+createVarsFileAnsible(varFileName, remoteJekyllPassWord)  
 
+provisionJekyllDevPlayBookNameAndPath=playBooksDir+"provisionJekyllDevServer.yaml"
+print("About to read the Playbook we will subsequently run. ")
+print("provisionJekyllDevPlayBookNameAndPath is: ", provisionJekyllDevPlayBookNameAndPath)
+f = open(provisionJekyllDevPlayBookNameAndPath, "r")	
+print(f.read())
+  
+playBookCommandSlice = "ansible-playbook -vvv "
+provisionJekyllDevCommand = playBookCommandSlice + provisionJekyllDevPlayBookNameAndPath
+print("provisionJekyllDevCommand is: ", provisionJekyllDevCommand)
+depfunc.runShellCommand(provisionJekyllDevCommand)
